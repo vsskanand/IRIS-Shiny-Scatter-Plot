863490059e1ffd51d2f85e3295b403ac96a73e9f
vsskanand <anand_fun30@yahoo.com>
2015-03-24 15:52:38 +0530

    Scatterplots of IRIS dataset

---
+++ server.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(ggplot2)
+ 
+shinyServer(function(input, output) {
+  #reactive() lesson 6 shiny-tutorial
+  dataset=reactive(iris[(iris$Sepal.Length>=input$Sepal.Length[1] & iris$Sepal.Length<=input$Sepal.Length[2]&
+                         iris$Sepal.Width>=input$Sepal.Width[1] & iris$Sepal.Width<=input$Sepal.Width[2]&
+                         iris$Petal.Length>=input$Petal.Length[1] & iris$Petal.Length<=input$Petal.Length[2]&
+                         iris$Petal.Width>=input$Petal.Width[1] & iris$Petal.Width<=input$Petal.Width[2]),])
+                            
+  #renderPlot() lesson 6 shiny-tutorial
+  output$scatterplot=renderPlot(ggplot(width = "500px", height = "900px",dataset(),aes_string(x=input$xvar, y=input$yvar,color="Species"))+ geom_point()+
+    ggtitle("Scatter Plot")
+    )
+
+
+})
---
+++ ui.R
@@ -0,0 +1,39 @@
+library(shiny)
+ 
+shinyUI(fluidPage(br(),
+  titlePanel("Building a Shiny App using IRIS Dataset"),
+  br(),
+
+  sidebarLayout(
+      
+
+      sidebarPanel(
+
+            h5("Creating scatteplot using iris dataset"), 
+             helpText("Please slide inorder to select a range of values."),
+            
+              sliderInput("Sepal.Length", label = "Sepal.Length Range", min = min(iris$Sepal.Length), 
+                         max = max(iris$Sepal.Length),value=c(min(iris$Sepal.Length),max(iris$Sepal.Length))),
+             
+              sliderInput("Sepal.Width", label = "Sepal.Width Range", min = min(iris$Sepal.Width), 
+                         max = max(iris$Sepal.Width), value = c(min(iris$Sepal.Width),max(iris$Sepal.Width))),
+             
+             
+             sliderInput("Petal.Length", label = "Petal.Length Range", min = min(iris$Petal.Length), 
+                         max = max(iris$Petal.Length), value = c(min(iris$Petal.Length),max(iris$Petal.Length))),
+             
+             
+             sliderInput("Petal.Width", label = "Petal.Width Range", min = min(iris$Petal.Width), 
+                         max = max(iris$Petal.Width), value = c(min(iris$Petal.Width),max(iris$Petal.Width))),
+             
+             h5("Select inputs for scatter plot"),
+             selectInput("xvar", "x-variable:", choices=names(iris[,-5])),
+             selectInput("yvar", "y-variable:", choices=names(iris[,-5]),selected = names(iris[2])) ),
+   
+    
+    mainPanel(plotOutput("scatterplot"),br(),br(),br(),tags$tfoot("By : vsskanand"))
+    
+    
+          )
+    )
+)
